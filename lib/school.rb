94872d390beceafd5c425080d4ebee8db41df081
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-08-12 15:40:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  
+  # attr_accessor :roster
+  
+  def initialize(roster)
+    @roster = {}
+  end
+  
+  @roster["9"] = []
+  @roster["10"] = []
+  @roster["11"] = []
+  @roster["12"] = []
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(grade, name)
+    @roster[:grade] << "name"
+  end
+
+end
+
+# school = School.new("Bayside High School")
